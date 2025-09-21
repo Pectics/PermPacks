286e7ef56c91696bfd09cdb587dc174597c96541
Pectics <pectics@outlook.com>
2025-09-21 23:46:44 +0800

    add: settings.gradle.kts中的阿里云Central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "PermPacks"
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositories {
+        maven("https://maven.aliyun.com/repository/central")
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/") {
             name = "papermc-repo"
