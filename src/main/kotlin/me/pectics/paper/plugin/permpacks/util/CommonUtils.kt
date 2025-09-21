a1a74c862333f22ee706cd2ef771af5105e92406
Pectics <pectics@outlook.com>
2025-09-22 00:30:39 +0800

    Fix: File.sha1()的线程安全问题

--- src/main/kotlin/me/pectics/paper/plugin/permpacks/util/CommonUtils.kt
+++ src/main/kotlin/me/pectics/paper/plugin/permpacks/util/CommonUtils.kt
@@ -5,23 +5,22 @@ import java.io.File
 import java.security.MessageDigest
 import java.util.Locale.getDefault
 
-private val SHA_1 = MessageDigest.getInstance("SHA-1")
-
 fun File.validate() {
     if (exists() && isFile && canRead()) return
     throw IllegalArgumentException("Invalid file: $path")
 }
 
 fun File.sha1(): Sha1Hex {
+    val sha1 = MessageDigest.getInstance("SHA-1")
     inputStream().use { fis ->
         val buf = ByteArray(DEFAULT_BUFFER_SIZE)
         while (true) {
             val read = fis.read(buf)
             if (read <= 0) break
-            SHA_1.update(buf, 0, read)
+            sha1.update(buf, 0, read)
         }
     }
-    return SHA_1.digest()
+    return sha1.digest()
         .joinToString("", transform = "%02x"::format)
         .let(Sha1Hex::of)
         .getOrThrow()
