de58e8c48b3bb67978442a1f1e92bcd04b517703
Pectics <pectics@outlook.com>
2025-09-26 11:26:44 +0800

    优化: 资源包清理逻辑 优化: 部分函数命名规范 优化: 注释本地化

--- src/main/kotlin/me/pectics/paper/plugin/permpacks/PermPacks.kt
+++ src/main/kotlin/me/pectics/paper/plugin/permpacks/PermPacks.kt
@@ -2,6 +2,8 @@ package me.pectics.paper.plugin.permpacks
 
 import com.comphenix.protocol.ProtocolLibrary
 import me.pectics.paper.plugin.permpacks.command.Commands
+import me.pectics.paper.plugin.permpacks.data.FilePackItem
+import me.pectics.paper.plugin.permpacks.data.Pack
 import me.pectics.paper.plugin.permpacks.packet.PackBlocker
 import me.pectics.paper.plugin.permpacks.packet.PackPacketTracker
 import me.pectics.paper.plugin.permpacks.upload.FileMetaRepository
@@ -14,16 +16,18 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class PermPacks : JavaPlugin() {
 
-    fun uploadServiceInit() {
+    fun initUploadService() {
+        // 检查文件上传配置
         if (!Options.fileUploadEnabled) return
         val service = Options.fileUploadService?.lowercase() ?: run {
             logger.warning("File upload service is not specified.")
             return
         }
-        // Catching service exceptions
+        // 获取配置上下文
         val context = Options.fileUploadContext
             ?.let(UploadServiceContext::of)
             ?: UploadServiceContext.EMPTY
+        // 初始化上传服务，捕获异常
         runCatching {
             when (service) {
                 in SelfHostService.names -> UploadService.initialize(SelfHostService, context)
@@ -36,24 +40,25 @@ class PermPacks : JavaPlugin() {
     }
 
     override fun onEnable() {
-        // Initialize components
+        // 初始化基础组件
         BinaryCache.initialize(this)
         FileMetaRepository.initialize(this)
         Options.initialize(this)
 
-        // Initialize upload service
-        uploadServiceInit()
+        initUploadService()
 
-        // Load packs
-        Options.loadPacks()
+        // 读取资源包配置
+        Options.readPacks()
 
-        // Cleanup remote uploads if enabled
-        if (Options.fileUploadEnabled && Options.uploadCleanupEnabled) {
-            val retain = Options.retainHashes()
+        // 清理过期的上传文件
+        if (Options.fileUploadEnabled && Options.fileUploadCleanup) {
+            val retain = Options.packs
+                .flatMap(Pack::items)
+                .filterIsInstance<FilePackItem>()
             UploadService.cleanup(retain)
         }
 
-        // Hook ProtocolLib if available
+        // 集成 ProtocolLib 以拦截资源包相关数据包
         server.pluginManager.getPlugin("ProtocolLib")
             ?.let {
                 if (Options.blockOtherPacks) {
@@ -65,6 +70,7 @@ class PermPacks : JavaPlugin() {
             }
             ?: logger.warning("ProtocolLib not found, some features may not work properly.")
 
+        // 注册命令
         Commands.register()
     }
 
